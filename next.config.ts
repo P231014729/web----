e1f52ff338626a4c9a097a3bb9ca4e6d12e13ed5
P231014729 <p231014729@stu.xbmu.edu.cn>
2025-07-10 12:02:07 +0800

    remove: 移除硬编码的 API Key

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,23 @@ const nextConfig: NextConfig = {
         hostname: 'source.unsplash.com',
       }
     ]
+  }
+};
+
+export default nextConfig;
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'source.unsplash.com',
+      }
+    ]
   },
   env: {
     WAKATIME_API_KEY: 'waka_ec86e50f-123f-4687-8b6a-0c39cbef98ec'
